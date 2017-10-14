0846e83ae4ce426bf08e82ebe079ac9ba1462c89
Sergi21r <sergi21r@gmail.com>
2017-10-14 15:07:07 +0200

    handout5 add & draw map

---
+++ Dev_class5_handout/Game/maps/CollisionTiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CollisionTiles" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="4.png"/>
+ </tile>
+</tileset>
---
+++ Dev_class5_handout/Game/maps/TiledExtreme.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ExtremeFootball" tilewidth="128" tileheight="128" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image width="64" height="64" source="Finish.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="Rect.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="DiagonalA.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="DiagonalP.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="5.png"/>
+ </tile>
+</tileset>
