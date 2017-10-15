e153486a494e88bc98f4aeab46bced8882fbed5d
Sergi21r <sergi21r@gmail.com>
2017-10-15 17:19:59 +0200

    try animate

--- Dev_class5_handout/Motor2D/j1Player.h
+++ Dev_class5_handout/Motor2D/j1Player.h
@@ -5,6 +5,7 @@
 #include "p2List.h"
 #include "p2Point.h"
 #include "j1Textures.h"
+#include "j1Animation.h"
 
 struct SDL_texture;
 struct SDL_Rect;
@@ -58,19 +59,23 @@ public:
 	fPoint				position;
 
 
-private:
-	p2SString name=nullptr;
-	unsigned int width=0;
-	unsigned int height=0;
+public:
 	
-	SDL_Texture* graph=nullptr;
+	p2SString name = nullptr;
+	unsigned int width = 0;
+	unsigned int height = 0;
+	
+	Animation* this_animation = nullptr;
+	Animation run;
+	Animation jump;
+	uint gid;
+	int speed;
+
+
+	SDL_Texture* graph = nullptr;
 
 	STATUS status;
 	STATUS2 status2;
-
-
-
-
 
 };
 	
